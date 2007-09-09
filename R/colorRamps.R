1ec94c068fe8f492b284eb4cd4e61610dd96fa30
Tim Keitt <tkeitt@gmail.com>
2007-09-09 00:00:00 +0000

    version 2.0

--- R/colorRamps.R
+++ R/colorRamps.R
@@ -1,51 +1,77 @@
-blue2red <- function(n)
+table.ramp <- function(n, mid = 0.5, sill = 0.5, base = 1)
   {
-    n2 <- ceiling(n / 2)
-    red <- rep(c(0, 1), each = n2)[1:n]
-    green <- 1 - abs(seq(-1, 1, length.out = n))
-    blue <- rev(red)
-    rgb(red, green, blue)
+    x <- seq(0, 1, length.out = n)
+    y <- rep(0, length(x))
+    sill.min <- max(c(1, round((n - 1) * (mid - sill / 2)) + 1))
+    sill.max <- min(c(n, round((n - 1) * (mid + sill / 2)) + 1))
+    y[sill.min:sill.max] <- 1
+    base.min <- round((n - 1) * (mid - base / 2)) + 1
+    base.max <- round((n - 1) * (mid + base / 2)) + 1
+    xi <- base.min:sill.min
+    yi <- seq(0, 1, length.out = length(xi))
+    i <- which(xi > 0 & xi <= n)
+    y[xi[i]] <- yi[i]
+    xi <- sill.max:base.max
+    yi <- seq(1, 0, length.out = length(xi))
+    i <- which(xi > 0 & xi <= n)
+    y[xi[i]] <- yi[i]
+    y
   }
+
+rgb.tables <- function(n,
+                       red = c(0.75, 0.25, 1),
+                       green = c(0.5, 0.25, 1),
+                       blue = c(0.25, 0.25, 1))
+  {
+    rr <- do.call("table.ramp", as.list(c(n, red)))
+    gr <- do.call("table.ramp", as.list(c(n, green)))
+    br <- do.call("table.ramp", as.list(c(n, blue)))
+    rgb(rr, gr, br)
+  }
+
+matlab.like <- function(n) rgb.tables(n)
+
+matlab.like2 <- function(n)
+  rgb.tables(n,
+             red = c(0.8, 0.2, 1),
+             green = c(0.5, 0.4, 0.8),
+             blue = c(0.2, 0.2, 1))
+
+blue2green2red <- matlab.like2
+
+blue2red <- function(n)
+  rgb.tables(n,
+             red = c(1, 1, 1),
+             green = c(0.5, 0, 1),
+             blue = c(0, 1, 1))
 
 green2red <- function(n)
-  {
-    n2 <- ceiling(n / 2)
-    red <- rep(c(0, 1), each = n2)[1:n]
-    blue <- 1 - abs(seq(-1, 1, length.out = n))
-    green <- rev(red)
-    rgb(red, green, blue)
-  }
+  rgb.tables(n,
+             red = c(1, 0, 2),
+             green = c(0, 0, 2),
+             blue = c(0, 0, 0))
 
 blue2green <- function(n)
-  {
-    n2 <- ceiling(n / 2)
-    green <- rep(c(0, 1), each = n2)[1:n]
-    red <- 1 - abs(seq(-1, 1, length.out = n))
-    blue <- rev(green)
-    rgb(red, green, blue)
-  }
+  rgb.tables(n,
+             red = c(0, 0, 0),
+             green = c(1, 0, 2),
+             blue = c(0, 0, 2))
 
-purple2green <- function(n)
-  {
-    red <- rep(0.5, length.out = n)
-    green <- seq(0, 1, length.out = n)
-    blue <- rev(green)
-    rgb(red, green, blue)
-  }
+magenta2green <- function(n)
+  rgb.tables(n,
+             red = c(0, 0, 2),
+             green = c(1, 0, 2),
+             blue = c(0, 0, 2))
 
 blue2yellow <- function(n)
-  {
-    red <- seq(0, 1, length.out = n)
-    green <- red
-    blue <- rev(red)
-    rgb(red, green, blue)
-  }
+  rgb.tables(n,
+             red = c(1, 0, 2),
+             green = c(1, 0, 2),
+             blue = c(0, 0, 2))
 
-cyan2magenta <- function(n)
-  {
-    red <- seq(0, 1, length.out = n)
-    green <- rev(red)
-    blue <- rep(1, n)
-    rgb(red, green, blue)
-  }
+cyan2yellow <- function(n)
+  rgb.tables(n,
+             red = c(1, 0, 2),
+             green = c(0.5, 1, 2),
+             blue = c(0, 0, 2))
 
